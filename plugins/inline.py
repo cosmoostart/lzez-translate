31840337061678b357838f23eb90007b4ed4b32f
cosmoostart <74989839+cosmoostart@users.noreply.github.com>
2021-10-12 23:28:12 +0500

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -20,15 +20,15 @@ async def inline(_, query: InlineQuery):
         		cd = splitit[1].lower().replace(" ", "")
         		text = splitit[0]
         except:
-        	METHOD=  [ InlineQueryResultArticle( title="How To Use", input_message_content=InputTextMessageContent("Ex ; ```@Googletranslateitbot how to use # hi```"),description="{Text}#{language code} ",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
-        	await query.answer( results=METHOD,  cache_time=2, switch_pm_text="Using Method",switch_pm_parameter="start" )
+        	METHOD=  [ InlineQueryResultArticle( title="How to use ❓", input_message_content=InputTextMessageContent("Ex ; ```@TranslateWoBot how to use # hi```"),description="{Text}#{language code} ",thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
+        	await query.answer( results=METHOD,  cache_time=2, switch_pm_text="Using method",switch_pm_parameter="start" )
         	return
         try:
         	translator = Translator()
         	translation = translator.translate(text,dest = cd)
         except:
         	return
-        TRTEXT=  [ InlineQueryResultArticle( title=f"Translated from {translation.src} To {translation.dest}", input_message_content=InputTextMessageContent(translation.text),description=f'{translation.text}',thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
+        TRTEXT=  [ InlineQueryResultArticle( title=f"Translated from {translation.src} to {translation.dest}", input_message_content=InputTextMessageContent(translation.text),description=f'{translation.text}',thumb_url="https://tg-link.herokuapp.com/dl/0/AgADh60xG50-wFc.jpg")]
         try:
         	await query.answer(results=TRTEXT ,  cache_time=2, switch_pm_text="Google Translater",switch_pm_parameter="start")
         except:
